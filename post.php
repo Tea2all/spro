bf122c74330053862bda1d65e3768244c2b18fd5
tea4all <admin@ecode.co.il>
2018-01-08 11:37:36 +0100

    added \n for new line after massage added to file

--- post.php
+++ post.php
@@ -4,10 +4,9 @@ require_once ('conf.php');
 
 if(isset($_POST['massage'])){
 $myfile = fopen("text.txt", "a") or die("Unable to open file!");
-$txt = en($_POST['massage'],$xpass);
+$txt = en($_POST['massage'],$xpass)."\n";
 fwrite($myfile, $txt);
 fclose($myfile);
-
 }
 
 ?>
